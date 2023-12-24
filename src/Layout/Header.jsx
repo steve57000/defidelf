4a70ce03369a071f0c81ef208e60a7c60bbf2c02
Bell Steve <steve57b@hotmail.fr>
2023-12-24 12:40:28 +0100

    add header img

--- src/Layout/Header.jsx
+++ src/Layout/Header.jsx
@@ -1,7 +1,11 @@
 import '../App.css';
+import Logo from '../logo.png'
+
 function Header() {
     return (
-        <header className="App-header"></header>
+        <header className="App-header">
+            <img className={"Logo"} src={Logo} alt={""} width={"150px"}></img>
+        </header>
     )
 }
 export default Header

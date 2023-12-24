436d4291e6ff7fa7ac1661d9781c5309c2ebd52e
Bell Steve <steve57b@hotmail.fr>
2023-12-24 12:41:51 +0100

    created home page

---
+++ src/Pages/HomePage/HomePage.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import '../../assets/styles/home.css';
+import TextArc from "../../Components/D3TextArc/TextArc";
+import logo from "../../logo1.png";
+
+
+function HomePage() {
+    // State (état, données)
+    // Comportement
+    // affichage (render)
+    return (
+        <div id="homePage">
+            <h1 id="title-logo">Page d'acceuil</h1>
+        </div>
+    );
+}
+
+export default HomePage;

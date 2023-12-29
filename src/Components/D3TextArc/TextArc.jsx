8bc7185c8e614359817d865f6d6b1e09b99b4b1d
Bell Steve <steve57b@hotmail.fr>
2023-12-29 18:18:54 +0100

    test

--- src/Components/D3TextArc/TextArc.jsx
+++ src/Components/D3TextArc/TextArc.jsx
@@ -29,7 +29,7 @@ function TextArc() {
                         <stop offset="30%" stopColor="#0b0979" stopOpacity="100%" />
                         <stop offset="100%" stopColor="#ff0000" stopOpacity="100%" />
                     </linearGradient>
-                    <path id="wavy" d="M 0,150, A 100,0 0 0,1 320,150" style={{fill: 'none', stroke: 'none'}}></path>
+                    <path id="wavy" d="M 0,150, A 100,0 0 0,1 320,150" style={{fill: 'none', stroke: 'black'}}></path>
                     <text>
                         <textPath href="#wavy" id="text-svg" startOffset="50%" style={{textAnchor: "middle", fill: "url(#linear-gradient)", filter: "drop-shadow(-2px 0 1px rgba(0,0,0,0.8)" , fontSize: '3.5em'}}>
                             DÉFI-DELF

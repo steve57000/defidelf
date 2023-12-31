3732d4d44b45ce7180c55bc352aa99a2d93838a0
Bell Steve <steve57b@hotmail.fr>
2023-12-31 12:16:37 +0100

    test

--- src/Components/D3TextArc/TextArc.jsx
+++ src/Components/D3TextArc/TextArc.jsx
@@ -6,11 +6,12 @@ function TextArc() {
     const svgRef = useRef(null);
     // Comportement
     useEffect(() => {
-        const width = "100%"
+        const width = 200
         const height = 200
+        const margin = {top: 0, right: 20, bottom: 0, left: 20}
         //Create the SVG
         const svg = d3.select(svgRef.current)
-        .attr('width', width)
+        .attr('width', width + margin.left + margin.right)
         .attr('height', height)
         .attr('overflow', 'visible')
         .attr('display', 'block')
@@ -31,7 +32,7 @@ function TextArc() {
                         <stop offset="30%" stopColor="#0b0979" stopOpacity="100%" />
                         <stop offset="100%" stopColor="#ff0000" stopOpacity="100%" />
                     </linearGradient>
-                    <path id="wavy" d="M 0,200, A 100,0 0 0,1 200,200" style={{fill: 'none', stroke: 'none', fontSize: '3.5em', padding: '10px'}}></path>
+                    <path id="wavy" d="M -20,200, A 100,0 0 0,1 220,200" style={{fill: 'none', stroke: 'blue', fontSize: '3.5em'}}></path>
                     <text>
                         <textPath href="#wavy" id="text-svg" startOffset="50%" style={{textAnchor: "middle", fill: "url(#linear-gradient)", filter: "drop-shadow(-2px 0 1px rgba(0,0,0,0.8)" , fontSize: '3.5em', padding: '10px'}}>
                             DÉFI-DELF

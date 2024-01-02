c2e5834d77dd8ac7c70ff836376743903c65fdec
Bell Steve <steve57b@hotmail.fr>
2024-01-02 13:09:49 +0100

    clean code

--- src/Components/D3TextArc/TextArc.jsx
+++ src/Components/D3TextArc/TextArc.jsx
@@ -6,12 +6,11 @@ function TextArc() {
     const svgRef = useRef(null);
     // Comportement
     useEffect(() => {
-        const width = 360
+        const width = 300
         const height = 200
-        const margin = {top: 0, right: 20, bottom: 0, left: 20}
         //Create the SVG
         const svg = d3.select(svgRef.current)
-        .attr('width', width + margin.left + margin.right)
+        .attr('width', width)
         .attr('height', height)
         .attr('overflow', 'visible')
         .attr('display', 'block')
@@ -19,7 +18,7 @@ function TextArc() {
 //Transition from the original arc path to a new arc path
         svg.selectAll("path")
         .transition().duration(4000).delay(1000)
-        .attr("d", "M 90,160 A80,80 0 0,1 310,160");
+        .attr("d", "M 40,160 A80,80 0 0,1 260,160");
     }, []);
 
 // affichage (render)
@@ -31,9 +30,9 @@ function TextArc() {
                     <stop offset="30%" stopColor="#0b0979" stopOpacity="100%" />
                     <stop offset="100%" stopColor="#ff0000" stopOpacity="100%" />
                 </linearGradient>
-                <path id="wavy" d="M 90,200, A 100,0 0 0,1 310,200" style={{fill: 'none', stroke: 'blue', fontSize: '3em', margin: '0 10px', overflow: 'visible', display: 'block'}}></path>
-                <text>
-                    <textPath href="#wavy" id="text-svg" startOffset="50%" style={{textAnchor: "middle", fill: "url(#linear-gradient)", filter: "drop-shadow(-2px 0 1px rgba(0,0,0,0.8)" , fontSize: '3.5em', padding: '10px'}}>
+                <path id="wavy" d="M 0,200, A 100,0 0 0,1 300,200" ></path>
+                <text style={{textAnchor: "middle", fill: "url(#linear-gradient)", filter: "drop-shadow(-2px 0 1px rgba(0,0,0,0.8)" , fontSize: '3.5em'}}>
+                    <textPath href="#wavy" id="text-svg" startOffset="50%" >
                         DÉFI-DELF
                     </textPath>
                 </text>
